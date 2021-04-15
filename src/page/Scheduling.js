59504788350946329050258bd24b24b50aed9241
mint47 <dor012299@gmail.com>
2021-04-14 23:49:21 -0400

    small fixes to scheduling, now doesnt update order.readyAt when changing date/time before clicking on confirm, also doesnt allow user to select a time prior to readyAt

--- src/page/Scheduling.js
+++ src/page/Scheduling.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { Container, Button } from '@material-ui/core';
+import { Container, Button, TableContainer } from '@material-ui/core';
 import Calendar from 'react-calendar';
 import TimeKeeper from 'react-timekeeper';
 import { orders } from "../App";
 import { useHistory } from "react-router";
+import { getAllByTestId } from "@testing-library/dom";
 
 
 
@@ -13,20 +14,28 @@ const orderFor = (orderNumber) => {
 }
 
 export function Schedule(props) {
-
     const history = useHistory();
     const [ order ] = useState(orderFor(props.location.state.orderNumber));
-    const [ date, setDate ] = useState(order.readyAt);
-    const [ time, setTime ] = useState("00:00");
+    const [ date, setDate ] = useState(new Date(order.readyAt));
+    const [ time, setTime ] = useState(date.toTimeString().substring(0,5));
+
 
     const changeDate = newDate => {
         setDate(newDate);
+        console.log("changeDate()->"+date);
+        //console.log(order.readyAt);
     };
 
-    const changeTime = time => {
-        const hours = parseInt(time.split(':')[0]), mins = parseInt(time.split(':')[1]);
+    const changeTime = newTime => {
+        const hours = parseInt(newTime.split(':')[0]), mins = parseInt(newTime.split(':')[1]);
         date.setHours(hours);
         date.setMinutes(mins);
+        console.log("changeTime()->"+time);
+        //console.log(order.readyAt);
+    }
+
+    const validPickUpDate = date => {
+        return date > order.readyAt ? true : false;
     }
 
     return (
@@ -34,7 +43,7 @@ export function Schedule(props) {
             <Container maxWidth="sm">
                 <Calendar
                     calendarType={"Hebrew"}
-                    minDate={date}
+                    minDate={order.readyAt}
                     minDetail={"month"}
                     next2Label={null}
                     prev2Label={null}
@@ -51,13 +60,17 @@ export function Schedule(props) {
             <Container maxWidth="sm">
                 <TimeKeeper
                     time = { time }
-                    onChange = { (newTime) => setTime(newTime.formatted24) }
+                    onChange = { (time) => setTime(time.formatted24)}
                     switchToMinuteOnHourSelect = {true}
                 />
                 {changeTime(time)}
             </Container>
 
-            <Button variant="contained" color="primary"
+            <Container>
+                {!validPickUpDate(date)? "Please pick a date after "+order.readyAt.toLocaleString():""}
+            </Container>
+
+            <Button disabled={!validPickUpDate(date)} variant="contained" color="primary"
                 onClick={() => {
                     order.readyAt = date;
                     history.push("/history");
